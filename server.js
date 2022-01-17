bd9c9ba94bdae3923d5a3bde57996f0c95497151
govindhansv <govindhansv333@gmail.com>
2022-01-17 16:39:26 +0530

    dd

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
-
 const PORT = 8000;
-
 const express = require('express');
 const app = express();
-
-// getting and posting methods
-
 app.get('/',(req,res)=>{
     res.send('Welcome to Backend! Bitch ')
 })
-
-
 app.listen(PORT,()=>{
   console.log(`server is listening on ${PORT}`);
 })
